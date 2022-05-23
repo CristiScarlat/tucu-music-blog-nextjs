ba8545b3433ca1a1d872eac617d38156922040af
CristiScarlat <78.cristi@gmail.com>
2022-05-23 11:16:16 +0300

    add reCaptcha for post write

--- components/modalForm/modalForm.jsx
+++ components/modalForm/modalForm.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
 import { Modal, Form, Button } from "react-bootstrap";
 import { toFirebaseTimestamp } from "../../services/api";
+import ReCAPTCHA from "react-google-recaptcha";
 
 const ModalForm = ({ showModal, handleCloseModal, handleSaveButton }) => {
+  const [formValues, setFormValues] = useState({});
+  const [captcha, setCaptcha] = useState(false);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const formTitle = e.target[0].value || "";
@@ -22,31 +27,68 @@ const ModalForm = ({ showModal, handleCloseModal, handleSaveButton }) => {
     return;
   };
 
+  const handleRealTimeValidation = (e) => {
+    const temp = { ...formValues };
+    temp[e.target.name] = e.target.value;
+    setFormValues(temp);
+  };
+
+  const getButtonDisabledStatus = () => {
+    if(!captcha)return true;
+    const values = Object.values(formValues);
+    const filtered = values.filter((v) => v !== "");
+    return filtered.length === 0;
+  };
+
+  const onCaptchaChange = () => {
+    setCaptcha(true);
+  };
+
   return (
     <Modal
       show={showModal}
       onHide={handleCloseModal}
       className="modal-content-dark-bg"
     >
-      <Modal.Header closeButton>
+      <Modal.Header closeButton style={{ color: "white" }}>
         <Modal.Title>Adauga impresia ta</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <Form onSubmit={handleSubmit}>
+        <Form onSubmit={handleSubmit} onChange={handleRealTimeValidation}>
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
             <Form.Label>Titlu</Form.Label>
-            <Form.Control type="text" placeholder="Titlu" autoFocus />
+            <Form.Control
+              type="text"
+              placeholder="Titlu"
+              autoFocus
+              name="title"
+            />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
             <Form.Label>Nume</Form.Label>
-            <Form.Control type="text" placeholder="Numele tau" autoFocus />
+            <Form.Control
+              type="text"
+              placeholder="Numele tau"
+              name="fullName"
+            />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
             <Form.Label>Detalii</Form.Label>
-            <Form.Control as="textarea" rows={5} />
+            <Form.Control
+              as="textarea"
+              rows={5}
+              maxLength={500}
+              name="details"
+            />
           </Form.Group>
+          <div className="mt-2 mb-2">
+            <ReCAPTCHA
+              sitekey={process.env.NEXT_PUBLIC_CAPTCHA_KEY}
+              onChange={onCaptchaChange}
+            />
+          </div>
           <div className="d-flex justify-content-end">
             <Button
               className="me-3"
@@ -55,7 +97,11 @@ const ModalForm = ({ showModal, handleCloseModal, handleSaveButton }) => {
             >
               Renunta
             </Button>
-            <Button type="submit" variant="primary">
+            <Button
+              type="submit"
+              variant={`${getButtonDisabledStatus() ? "secondary" : "primary"}`}
+              disabled={getButtonDisabledStatus()}
+            >
               Salveaza
             </Button>
           </div>
